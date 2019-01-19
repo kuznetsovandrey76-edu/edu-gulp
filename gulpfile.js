f6c05ba6d51f4e4a8b1d99c6814b39641b216411
Andrey Kuznetsov <and.rey.q@yandex.ru>
2019-01-19 15:56:12 +0300

    add sprites

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,36 @@ var gulp = require('gulp'),
     cleanCSS = require('gulp-clean-css'),
     uglify = require('gulp-uglify'),
     del = require('del'),
+    spritesmith = require('gulp.spritesmith'),
     // watch = require('gulp-watch'),
+    merge = require('merge-stream'),
     browserSync = require('browser-sync').create(),
     reload = browserSync.reload;
 
+
+
+// SASS 
 var sass_src = 'resources/assets/sass/style.scss',
     sass_dist = 'public_html/assets/css',
     // watch
     sass_files = 'resources/assets/sass/**/*.scss';
 
+// JS 
 var js_src = 'resources/assets/js/main.js',
     js_dist = 'public_html/assets/js',
     // watch
     js_files = 'resources/assets/js/**/*.js';
+
+// SPRITES
+var sprite_src = 'resources/assets/img/sprites/*.png',
+    sprite_sass_path = 'resources/assets/sass',
+    sprite_img_dist = 'public_html/assets/img';
+
+// IMAGES
+var images_src = 'resources/assets/img/images/**/*.{png,jpg,gif}',
+    images_dist = 'public_html/assets/img';
+
+
 
 gulp.task('sass', function() {
     return gulp.src(sass_src)
@@ -39,10 +56,25 @@ gulp.task('js', function() {
         .pipe(browserSync.stream());
 });
 
+gulp.task('sprite', function () {
+    var spriteData = gulp.src(sprite_src)
+        .pipe(spritesmith({
+            imgName: '../img/sprite.png',
+            cssName: '_sprite.scss'
+    }));   
+
+    var imgStream = spriteData.img
+        .pipe(gulp.dest(sprite_img_dist));
+
+    var cssStream = spriteData.css
+        .pipe(gulp.dest(sprite_sass_path));
+
+    return merge(imgStream, cssStream);
+});
+
 gulp.task('clean', function() {
     return del(['public_html/assets/']);
 });
-
 
 gulp.task('watch', function() {
     browserSync.init({
@@ -56,7 +88,12 @@ gulp.task('watch', function() {
     gulp.watch('./public_html/**/*.html').on("change", reload);
 });
 
-gulp.task('build', gulp.series('clean', 
-                        gulp.parallel('sass', 'js')));
+
+gulp.task('build', gulp.series(
+    'clean', 
+    'sprite', 
+    'js', 
+    'sass'
+));
 
 gulp.task('dev', gulp.series('build', 'watch'));
